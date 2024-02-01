8fd05e685b54cea0f6a833e27e9e003e7c0b7c14
Max Reble <max.reble@aco.com>
2024-02-01 09:33:09 +0100

    Adds localization
    and test middleware to show the problem in the logs

--- Program.cs
+++ Program.cs
@@ -4,16 +4,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+	.AddInteractiveServerComponents();
+
+//Adds localization services (probably not needed for our approach)
+builder.Services.AddLocalization();
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Error", createScopeForErrors: true);
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -21,7 +25,24 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
+/* This middleware help us to understand the problem
+ * Every request happens with the correct accepted language
+ * expect of the last one with the Path /_blazor.
+ * The last one has "en-US,en;q=0.9,de;q=0.8"
+ */
+app.Use((ctx, next) =>
+{
+	var logger = ctx.RequestServices.GetRequiredService<ILogger<Program>>();
+	logger.LogInformation("Request Path: {Request}\n\tAccepted Language: {Header}", ctx.Request.Path,
+		ctx.Request.Headers.AcceptLanguage);
+	return next(ctx);
+});
+
+//Use request localization (--> locale in browser)
+app.UseRequestLocalization(["en-US", "de-DE"]);
+
+
 app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+	.AddInteractiveServerRenderMode();
 
 app.Run();
